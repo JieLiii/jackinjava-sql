b2e468d444a887a90366a630c4b572fe0e235ec5
venus_jieli <jie.li@venusource.com>
2019-07-24 17:29:46 +0800

    初始提交

---
+++ src/main/java/com/jackinjava/tools/jackinjava/sql/JackinjavaSqlApplication.java
@@ -0,0 +1,13 @@
+package com.jackinjava.tools.jackinjava.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackinjavaSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackinjavaSqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jackinjava/tools/jackinjava/sql/service/impl/ConnectionServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jackinjava.tools.jackinjava.sql.service.impl;
+
+import com.jackinjava.tools.jackinjava.sql.service.ConnectionService;
+
+public class ConnectionServiceImpl implements ConnectionService {
+
+}
